d84e115c9b73b74a5698630c1e51e61f8f880ce9
hyperdefined <13475823+hyperdefined@users.noreply.github.com>
2020-08-12 03:27:32 -0400

    Added reload permission

--- src/main/java/lol/hyper/ezhomes/commands/CommandReload.java
+++ src/main/java/lol/hyper/ezhomes/commands/CommandReload.java
@@ -10,8 +10,12 @@ import org.bukkit.configuration.file.YamlConfiguration;
 public class CommandReload implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        EzHomes.getInstance().config = YamlConfiguration.loadConfiguration(EzHomes.getInstance().configFile);
-        sender.sendMessage(ChatColor.GREEN + "Config was reloaded!");
+        if (sender.hasPermission("ezhomes.reload")) {
+            EzHomes.getInstance().config = YamlConfiguration.loadConfiguration(EzHomes.getInstance().configFile);
+            sender.sendMessage(ChatColor.GREEN + "Config was reloaded!");
+        } else {
+            sender.sendMessage(ChatColor.RED + "You don't have permission to reload!");
+        }
         return true;
     }
 }
