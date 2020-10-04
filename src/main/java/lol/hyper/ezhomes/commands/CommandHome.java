98dd7cb852b8bb8e5eb3de75b7a235cf4600ef78
hyperdefined <13475823+hyperdefined@users.noreply.github.com>
2020-10-04 16:42:22 -0400

    added tabcomplete for /home to list player homes

--- src/main/java/lol/hyper/ezhomes/commands/CommandHome.java
+++ src/main/java/lol/hyper/ezhomes/commands/CommandHome.java
@@ -6,17 +6,17 @@ import lol.hyper.ezhomes.HomeManagement;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.json.simple.parser.ParseException;
 
-import java.awt.print.Paper;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class CommandHome implements CommandExecutor {
+public class CommandHome implements TabExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
@@ -55,5 +55,16 @@ public class CommandHome implements CommandExecutor {
             }
         }
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        Player player = (Player) sender;
+        try {
+            return HomeManagement.getPlayerHomes(player);
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
