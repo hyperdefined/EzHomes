bb3a313886a877138abba1eb0f066c668595631b
hyperdefined <13475823+hyperdefined@users.noreply.github.com>
2021-02-16 12:09:49 -0500

    added bypass cooldown check on gui

--- src/main/java/lol/hyper/ezhomes/Events.java
+++ src/main/java/lol/hyper/ezhomes/Events.java
@@ -52,7 +52,7 @@ public class Events implements Listener {
             event.setCancelled(true);
             ItemStack item = event.getCurrentItem();
             if (item.getType() == Material.RED_BED && item.getType() != Material.AIR) {
-                if (ezHomes.homeManagement.canPlayerTeleport(player.getUniqueId())) {
+                if (ezHomes.homeManagement.canPlayerTeleport(player.getUniqueId()) || player.hasPermission("ezhomes.bypasscooldown")) {
                     ItemMeta meta = item.getItemMeta();
                     Location loc = ezHomes.homeManagement.getHomeLocation(player.getUniqueId(), meta.getDisplayName());
                     PaperLib.teleportAsync(player, loc);
